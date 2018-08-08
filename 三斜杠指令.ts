a50acf6d2eb05a29aef95076f4df8f558fad3373
奕盛 <yicheng@yishengdeMacBook-Pro.local>
2018-08-08 22:50:42 +0800

    同步

---
+++ 三斜杠指令.ts
@@ -0,0 +1,7 @@
+/**
+ * /// <reference path="三斜杠指令1.ts" /> 相关联的ts文件一起编译为js
+ * /// <reference no-default-lib="true"/> 编译器不编译该ts文件
+ * ///<amd-module name="NamedModule"/>  作用未知
+ * /// <reference types="..." />   作用未知
+ */
+
